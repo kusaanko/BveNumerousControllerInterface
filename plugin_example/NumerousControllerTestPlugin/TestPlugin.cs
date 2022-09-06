bdfcf9f3051c42843d1cbc713485ccfd6c8da61c
kusaanko <39370373+kusaanko@users.noreply.github.com>
2022-09-06 19:44:18 +0900

    Update TestPlugin.cs

--- plugin_example/NumerousControllerTestPlugin/TestPlugin.cs
+++ plugin_example/NumerousControllerTestPlugin/TestPlugin.cs
@@ -8,13 +8,16 @@ using System.Windows.Forms;
 
 namespace NumerousControllerTestPlugin
 {
+    // 空っぽのプラグイン。何もしないコントローラーを追加します。
     public class TestPlugin : NumerousControllerPlugin
     {
+        // プラグイン終了時の処理
         public void Dispose()
         {
 
         }
 
+        // コントローラー追加
         public List<NCIController> GetAllControllers()
         {
             List<NCIController> controllers = new List<NCIController>();
@@ -22,26 +25,31 @@ namespace NumerousControllerTestPlugin
             return controllers;
         }
 
+        // プラグイン名
         public string GetName()
         {
             return "TestPlugin";
         }
 
+        // バージョン名
         public string GetVersion()
         {
             return "1.0";
         }
 
+        // プラグイン読み込み時
         public void Load()
         {
 
         }
 
+        // プラグイン読み込み時の設定ファイルの読み込み。BveTs/Settingsフォルダが引数に渡されます。
         public void LoadConfig(string directory)
         {
             MessageBox.Show(directory);
         }
 
+        // NumerousControllerInterfaceのプロパティ内の設定ボタンを押したときの動作です。
         public void ShowConfigForm()
         {
             new Form1().Show();
