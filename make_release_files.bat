eff7086583bbceca517d55adefa0c2701a11eeb9
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2024-03-21 18:10:48 +0900

    Update make_release_files.bat

--- make_release_files.bat
+++ make_release_files.bat
@@ -1,16 +1,14 @@
 cd src\bin\Release
 if not exist "Input Devices" mkdir "Input Devices"
 copy "Kusaanko.NumerousControllerInterface.dll" "Input Devices" /Y
-"C:\Program Files\7-Zip\7z.exe" a NumerousControllerInterface_Bve5.zip "Input Devices" LibUsbDotNet.dll
-cd ..\..\NumerousControllerInterface.NET4\bin\Release
-if not exist "Input Devices" mkdir "Input Devices"
-copy "Kusaanko.NumerousControllerInterface.NET4.dll" "Input Devices" /Y
-"C:\Program Files\7-Zip\7z.exe" a NumerousControllerInterface_Bve6.zip "Input Devices" LibUsbDotNet.dll
-cd ..\..\..\..\
+"C:\Program Files\7-Zip\7z.exe" a NumerousControllerInterface.zip "Input Devices" LibUsbDotNet.dll
+cd ..\..\..\
 if not exist "bin" mkdir "bin"
-copy "src\bin\Release\NumerousControllerInterface_Bve5.zip" "bin" /Y
-copy "src\NumerousControllerInterface.NET4\bin\Release\NumerousControllerInterface_Bve6.zip" "bin" /Y
-del "src\bin\Release\NumerousControllerInterface_Bve5.zip"
-del "src\NumerousControllerInterface.NET4\bin\Release\NumerousControllerInterface_Bve6.zip"
-"C:\Program Files\7-Zip\7z.exe" a "bin\NumerousControllerInterface_Bve5.zip" install.bat script.ps1
-"C:\Program Files\7-Zip\7z.exe" a "bin\NumerousControllerInterface_Bve6.zip" install.bat script.ps1
+copy "src\bin\Release\NumerousControllerInterface.zip" "bin" /Y
+del "src\bin\Release\NumerousControllerInterface.zip"
+cd Installer\bin\Release
+"C:\Program Files\7-Zip\7z.exe" a Installer.zip NumerousControllerInterfaceInstaller.exe NumerousControllerInterfaceInstaller.exe.config
+cd ..\..\..\
+copy "Installer\bin\Release\Installer.zip" "bin" /Y
+del "Installer\bin\Release\Installer.zip"
+pause
