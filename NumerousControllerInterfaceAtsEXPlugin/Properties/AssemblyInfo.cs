2a1e8c2e18e733c968b1dbe69584fe1f6358a6a3
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2024-03-23 19:37:20 +0900

    ワイルドカードを使用したバージョンに変更

--- NumerousControllerInterfaceAtsEXPlugin/Properties/AssemblyInfo.cs
+++ NumerousControllerInterfaceAtsEXPlugin/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AtsEX 拡張機能：NumerousControllerInterfaceAtsEXPlugin")]
 [assembly: AssemblyDescription("NumerousControllerInterfaceとAtsEXを連携させるためのプラグイン")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kusaanko")]
 [assembly: AssemblyProduct("NumerousControllerInterface AtsEXPlugin")]
 [assembly: AssemblyCopyright("Copyright © Kusaanko 2024")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
