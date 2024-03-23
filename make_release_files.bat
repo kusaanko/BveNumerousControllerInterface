73e4b134bf7721e90ebcc0d91ca4c8da96a14ec6
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2024-03-23 22:57:50 +0900

    AtsEXプラグインのzipファイルを作成するように

--- make_release_files.bat
+++ make_release_files.bat
@@ -9,6 +9,11 @@ del "src\bin\Release\NumerousControllerInterface.zip"
 cd Installer\bin\Release
 "C:\Program Files\7-Zip\7z.exe" a Installer.zip NumerousControllerInterfaceInstaller.exe NumerousControllerInterfaceInstaller.exe.config
 cd ..\..\..\
+copy "NumerousControllerInterfaceAtsEXPlugin\bin\Release\NumerousControllerInterfaceAtsEXPlugin.zip" "bin" /Y
+del "NumerousControllerInterfaceAtsEXPlugin\bin\Release\NumerousControllerInterfaceAtsEXPlugin.zip"
+cd NumerousControllerInterfaceAtsEXPlugin\bin\Release
+"C:\Program Files\7-Zip\7z.exe" a NumerousControllerInterfaceAtsEXPlugin.zip NumerousControllerInterfaceAtsEXPlugin.dll
+cd ..\..\..\
 copy "Installer\bin\Release\Installer.zip" "bin" /Y
 del "Installer\bin\Release\Installer.zip"
 "C:\Program Files\7-Zip\7z.exe" a bin/Installer.zip library_license.txt
