584f2b0b5a893143adc260d9db73642e0c8edcea
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2024-12-13 23:16:46 +0900

    Installer.zipからNumerousControllerInterfaceInstaller.zipへ

--- make_release_files.bat
+++ make_release_files.bat
@@ -7,12 +7,12 @@ if not exist "bin" mkdir "bin"
 copy "src\bin\Release\NumerousControllerInterface.zip" "bin" /Y
 del "src\bin\Release\NumerousControllerInterface.zip"
 cd Installer\bin\Release
-"C:\Program Files\7-Zip\7z.exe" a Installer.zip NumerousControllerInterfaceInstaller.exe NumerousControllerInterfaceInstaller.exe.config
+"C:\Program Files\7-Zip\7z.exe" a NumerousControllerInterfaceInstaller.zip NumerousControllerInterfaceInstaller.exe NumerousControllerInterfaceInstaller.exe.config
 cd ..\..\..\
-copy "Installer\bin\Release\Installer.zip" "bin" /Y
-del "Installer\bin\Release\Installer.zip"
-"C:\Program Files\7-Zip\7z.exe" a bin/Installer.zip library_license.txt
+copy "Installer\bin\Release\NumerousControllerInterfaceInstaller.zip" "bin" /Y
+del "Installer\bin\Release\NumerousControllerInterfaceInstaller.zip"
+"C:\Program Files\7-Zip\7z.exe" a bin/NumerousControllerInterfaceInstaller.zip library_license.txt
 "C:\Program Files\7-Zip\7z.exe" a bin/NumerousControllerInterface.zip library_license.txt
-"C:\Program Files\7-Zip\7z.exe" a bin/Installer.zip LICENSE
+"C:\Program Files\7-Zip\7z.exe" a bin/NumerousControllerInterfaceInstaller.zip LICENSE
 "C:\Program Files\7-Zip\7z.exe" a bin/NumerousControllerInterface.zip LICENSE
 pause
